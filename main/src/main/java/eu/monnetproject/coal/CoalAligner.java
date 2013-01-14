a60204a1b16f729bf203f7e8850debf347a42bad
John McCrae <john@mccr.ae>
2013-01-14 12:24:44 +0000

    Add similarity measures and example for Xichuan
    git-svn-id: https://subversion.deri.ie/monnet_dev/sources/trunk/coal@4283 18474ff0-dd96-4de7-8b2b-b9d0bd1f8700

--- main/src/main/java/eu/monnetproject/coal/CoalAligner.java
+++ main/src/main/java/eu/monnetproject/coal/CoalAligner.java
@@ -4,105 +4,131 @@ import java.util.List;
 
 import eu.monnetproject.align.Aligner;
 import eu.monnetproject.align.Alignment;
+import eu.monnetproject.align.AlignmentSerializer;
 import eu.monnetproject.align.Matcher;
 import eu.monnetproject.align.Match;
 import eu.monnetproject.coal.svmrank.SVMRankMatcher;
+import eu.monnetproject.framework.services.Services;
 import eu.monnetproject.ontology.Entity;
 import eu.monnetproject.ontology.Ontology;
+import eu.monnetproject.ontology.OntologySerializer;
 import java.io.File;
+import java.io.FileReader;
+import java.util.Collections;
 import java.util.logging.Logger;
 
 /**
  * Coal implementation of an Aligner.
- * 
+ *
  * @author Dennis Spohr
  *
  */
 public class CoalAligner implements Aligner {
 
-        private Logger log = Logger.getLogger(CoalAligner.class.getName());
-        private final Matcher coalMatcher;
-        public volatile int progress = 0;
+    private Logger log = Logger.getLogger(CoalAligner.class.getName());
+    private final Matcher coalMatcher;
+    public volatile int progress = 0;
 
     public CoalAligner(Matcher coalMatcher) {
         this.coalMatcher = coalMatcher;
     }
 
     public void reconfigure(File matcherCfg, File svmRankClassify, File svmRankLearn, File modelFile) {
-        if(coalMatcher instanceof SVMRankMatcher) {
-            ((SVMRankMatcher)coalMatcher).reconfigure(matcherCfg, svmRankClassify, svmRankLearn,modelFile);
+        if (coalMatcher instanceof SVMRankMatcher) {
+            ((SVMRankMatcher) coalMatcher).reconfigure(matcherCfg, svmRankClassify, svmRankLearn, modelFile);
         }
     }
-    
+
     public Alignment align(Ontology ontlg, Ontology ontlg1) {
         Alignment alignment = new CoalAlignment();
         align(ontlg1, ontlg1, alignment);
         return alignment;
     }
-        
-        
-        
-        public void align(Ontology srcOntology, Ontology tgtOntology,
-                Alignment alignment, boolean ignoreInstances) {
-        	
-        	this.progress = 0;
-        	
-        	if (srcOntology == null)
-        		log.severe("Source ontology is null!");
-        	if (tgtOntology == null)
-        		log.severe("Target ontology is null!");
-        	if (srcOntology == null || tgtOntology == null)
-        		System.exit(-1);
-        		
-        	log.info("Aligning "+srcOntology+ " and "+tgtOntology);
 
-        	alignment.setSourceOntology(srcOntology);
-        	alignment.setTargetOntology(tgtOntology);
-        	
-        	for(Entity ent1 : srcOntology.getEntities()) {
+    public void align(Ontology srcOntology, Ontology tgtOntology,
+            Alignment alignment, boolean ignoreInstances) {
 
-    			if (ignoreInstances) {
-    				if (ent1 instanceof eu.monnetproject.ontology.LiteralOrIndividual) {
-        				log.info("Skipping individual "+ent1.getURI().toString());
-    					this.progress++;
-    					continue;
-    				}
-    			}
-    			
-    			log.info("Processing "+ent1);
-    			
-    			List<Match> matches = this.coalMatcher.getMatches(ent1,tgtOntology,alignment.getMaximumNumberOfMatches());
+        this.progress = 0;
 
-    			try{
-        			alignment.addAllMatches(matches);
-        		} catch (IllegalArgumentException e) {
-        			log.warning("No matches for "+ent1.getURI());
-        		}
-        		
-        		this.progress++;
-        		
-        	}
-        	
+        if (srcOntology == null) {
+            log.severe("Source ontology is null!");
         }
-        
-        /**
-         * Align two ontologies and store the result in <code>alignment</code>.
-         * 
-         * @param srcOntology source ontology
-         * @param tgtOntology target ontology
-         * @param alignment alignment
-         */
-        @Override
-        public void align(Ontology srcOntology, Ontology tgtOntology,
-                        Alignment alignment) {
-
-        	align(srcOntology, tgtOntology, alignment, true);
-
+        if (tgtOntology == null) {
+            log.severe("Target ontology is null!");
         }
-        
-        public int getProgress() {
-        	return this.progress;
+        if (srcOntology == null || tgtOntology == null) {
+            System.exit(-1);
         }
 
+        log.info("Aligning " + srcOntology + " and " + tgtOntology);
+
+        alignment.setSourceOntology(srcOntology);
+        alignment.setTargetOntology(tgtOntology);
+
+        for (Entity ent1 : srcOntology.getEntities()) {
+
+            if (ignoreInstances) {
+                if (ent1 instanceof eu.monnetproject.ontology.LiteralOrIndividual) {
+                    log.info("Skipping individual " + ent1.getURI().toString());
+                    this.progress++;
+                    continue;
+                }
+            }
+
+            log.info("Processing " + ent1);
+
+            List<Match> matches = this.coalMatcher.getMatches(ent1, tgtOntology, alignment.getMaximumNumberOfMatches());
+
+            try {
+                alignment.addAllMatches(matches);
+            } catch (IllegalArgumentException e) {
+                log.warning("No matches for " + ent1.getURI());
+            }
+
+            this.progress++;
+
+        }
+
+    }
+
+    /**
+     * Align two ontologies and store the result in
+     * <code>alignment</code>.
+     *
+     * @param srcOntology source ontology
+     * @param tgtOntology target ontology
+     * @param alignment alignment
+     */
+    @Override
+    public void align(Ontology srcOntology, Ontology tgtOntology,
+            Alignment alignment) {
+
+        align(srcOntology, tgtOntology, alignment, true);
+
+    }
+
+    public int getProgress() {
+        return this.progress;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage:\tmvn exec:java -Dexec.mainClass=\"" + CoalAligner.class.getCanonicalName() + "\" -Dexec.args=\"ontology1 ontology2 output\"");
+            System.exit(-1);
+        }
+        final OntologySerializer serializer = Services.get(OntologySerializer.class);
+        final AlignmentSerializer alignSerializer = Services.get(AlignmentSerializer.class);
+        final Aligner aligner = Services.get(Aligner.class);
+        final Ontology ontology1 = serializer.read(new FileReader(args[0]));
+        final Ontology ontology2 = serializer.read(new FileReader(args[1]));
+        final Alignment alignment = aligner.align(ontology1, ontology2);
+        alignSerializer.writeAlignment(alignment, new File(args[2]));
         
+        // Print scores (for Xichuan)
+        for(Entity entity : alignment.getSourceEntities()) {
+            for(Match match : alignment.getMatches(entity)) {
+                System.out.println(match.getSourceEntity() + " -> " + match.getTargetEntity() + " (" + match.getScore() + ")");
+            }
+        }
+    }
 }
